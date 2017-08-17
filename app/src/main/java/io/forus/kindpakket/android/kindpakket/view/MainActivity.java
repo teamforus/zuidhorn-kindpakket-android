aee030ea785fc020148f31736d8dfa8c6c1f7324
Timon Back <T.Back@student.rug.nl>
2017-08-17 11:24:19 +0200

    Added MainActivity

---
+++ app/src/main/java/io/forus/kindpakket/android/kindpakket/view/MainActivity.java
@@ -0,0 +1,33 @@
+package io.forus.kindpakket.android.kindpakket.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import io.forus.kindpakket.android.kindpakket.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Button loginButton = (Button) findViewById(R.id.main_login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+        final Button registerButton = (Button) findViewById(R.id.main_register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+    }
+}
