e72e7762afbb5ee10d9e611ffc448102dd7db137
Timon Back <T.Back@student.rug.nl>
2017-08-17 12:55:11 +0200

    Added basic voucher processing

---
+++ app/src/main/java/io/forus/kindpakket/android/kindpakket/view/VoucherProcessActivity.java
@@ -0,0 +1,15 @@
+package io.forus.kindpakket.android.kindpakket.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.forus.kindpakket.android.kindpakket.R;
+
+public class VoucherProcessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voucher_process);
+    }
+}
