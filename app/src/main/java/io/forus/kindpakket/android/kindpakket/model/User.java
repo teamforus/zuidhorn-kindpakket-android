04e651a7fb053bea3690ab399ec96cd894abdfcb
Timon Back <T.Back@student.rug.nl>
2017-08-19 20:05:54 +0200

    Updated user model

--- app/src/main/java/io/forus/kindpakket/android/kindpakket/model/User.java
+++ app/src/main/java/io/forus/kindpakket/android/kindpakket/model/User.java
@@ -1,15 +1,35 @@
 package io.forus.kindpakket.android.kindpakket.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
 public class User {
     public static final User INVALID = null;
 
-    /**
-     * _token gets initalized by Retrofit
-     */
-    private String _token;
+    @SerializedName("user_id")
+    private int id;
+    private String name;
+    private String iban;
+    @SerializedName("kvk_number")
+    private String kvkNumber;
+    @SerializedName("phone_number")
+    private String phoneNumber;
+    private String state;
+    @SerializedName("created_at")
+    private Date createdAt;
+    @SerializedName("updated_at")
+    private Date updatedAt;
 
+    public String getName() {
+        return name;
+    }
 
-    public String getToken() {
-        return _token;
+    public String getIban() {
+        return iban;
+    }
+
+    public String getKvkNumber() {
+        return kvkNumber;
     }
 }
