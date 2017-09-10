d5f1f4cfc31070459b0d2ba5371e01c94f575a2b
Timon Back <T.Back@student.rug.nl>
2017-09-10 16:10:15 +0200

    Simplified further

--- app/src/androidTest/java/io/forus/kindpakket/android/kindpakket/view/LoginActivityTest.java
+++ app/src/androidTest/java/io/forus/kindpakket/android/kindpakket/view/LoginActivityTest.java
@@ -4,15 +4,11 @@ import android.content.Intent;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import io.forus.kindpakket.android.kindpakket.service.api.ApiFactory;
 import io.forus.kindpakket.android.kindpakket.util.ScreenshotUtil;
-import okhttp3.mockwebserver.MockWebServer;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -20,14 +16,6 @@ public class LoginActivityTest {
     @Rule
     public IntentsTestRule<LoginActivity> mActivityRule =
             new IntentsTestRule<>(LoginActivity.class, true, false);
-    private MockWebServer server;
-
-    @Before
-    public void setUp() throws Exception {
-        server = new MockWebServer();
-        server.start();
-        ApiFactory.build(server.url("/").toString());
-    }
 
     @Test
     public void render() {
@@ -35,10 +23,5 @@ public class LoginActivityTest {
         mActivityRule.launchActivity(intent);
 
         new ScreenshotUtil(mActivityRule.getActivity()).snap(mActivityRule.getActivity());
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        server.shutdown();
     }
 }
